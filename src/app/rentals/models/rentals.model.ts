093e6f6b1716876881765e4f191ac8bb8db077e6
THIQAHDEVa.mabdelwahed <m.attia@outlook.sa>
2020-08-14 22:42:26 +0200

    change rental dto

--- src/app/rentals/models/rentals.model.ts
+++ src/app/rentals/models/rentals.model.ts
@@ -1,49 +1,50 @@
 export interface RentalModel {
- "unit": RetnalDataModel,
-  "unitGroupNameAr": string,
+  unit: RetnalDataModel;
+  unitGroupNameAr: string;
 }
 
 interface RetnalDataModel {
-  "nameAr": string
-  "nameEn": string
-  "description": string
-  "location": string
-  "longitude": string
-  "latitude": string
-  "status": number,
-  "hasLight": boolean,
-  "hasWater": boolean,boolean
-  "hasInternet": boolean,
-  "hasParking": boolean,
-  "unitGroupId": number,
-  "id":number,
-  "images": Array<RentalImageModel>,
-  "video": Array<RentalVideoModel>,
-  "prices": Array<RentalPriceModel>
+  name: string;
+  description: string;
+  location: string;
+  longitude: string;
+  latitude: string;
+  status: number;
+  hasLight: boolean;
+  hasWater: boolean;
+  hasInternet: boolean;
+  hasParking: boolean;
+  isFamily: boolean;
+  isSingle: boolean;
+  unitGroupId: number;
+  id: number;
+  images: Array<RentalImageModel>;
+  video: Array<RentalVideoModel>;
+  prices: Array<RentalPriceModel>;
 }
 
 interface RentalImageModel {
-  "path": "https://q-xx.bstatic.com/xdata/images/hotel/840x460/124346702.jpg?k=73447c106017f39a9646d8c78fd9075f2c3d395e9686dd6fdbb0e4cd1268fc37&o=\t",
-  "type": null,
-  "isMain": true,
-  "isOnSlider": true,
-  "sliderOrder": 1,
-  "unitId": 1,
-  "id": 3
+  path: "https://q-xx.bstatic.com/xdata/images/hotel/840x460/124346702.jpg?k=73447c106017f39a9646d8c78fd9075f2c3d395e9686dd6fdbb0e4cd1268fc37&o=\t";
+  type: null;
+  isMain: true;
+  isOnSlider: true;
+  sliderOrder: 1;
+  unitId: 1;
+  id: 3;
 }
 
-interface RentalVideoModel{
-        "path": string,
-        "unitId": number,
-        "id": number
+interface RentalVideoModel {
+  path: string;
+  unitId: number;
+  id: number;
 }
 
-interface RentalPriceModel{
-  "dayPrice": number,
-  "weekEndPrice": string
-  "priceOrder": number,
-  "fromDate": string
-  "toDate": string
-  "unitId": number,
-  "id": number
+interface RentalPriceModel {
+  dayPrice: number;
+  weekEndPrice: string;
+  priceOrder: number;
+  fromDate: string;
+  toDate: string;
+  unitId: number;
+  id: number;
 }
