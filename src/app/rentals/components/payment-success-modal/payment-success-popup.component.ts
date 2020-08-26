4e497d325e26d3b87130712fa0850add915f8b61
THIQAHDEVa.mabdelwahed <m.attia@outlook.sa>
2020-08-26 23:24:45 +0200

    add new components

---
+++ src/app/rentals/components/payment-success-modal/payment-success-popup.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-paymnetsuccess",
+  templateUrl:'./payment-success-popup.component.html'
+})
+export class PaymentSuccessPopupComponent implements OnInit {
+
+  constructor() {}
+
+    ngOnInit() {
+    }
+
+}
--- src/app/rentals/rentals.module.ts
+++ src/app/rentals/rentals.module.ts
@@ -2,6 +2,8 @@ import { CommonModule } from "@angular/common";
 import { FormsModule } from '@angular/forms';
 import { NgModule } from "@angular/core";
 import { PaymentComponent } from './components/payment/payment.component';
+import { PaymentNoticePopupComponent } from './components/payment-notice-popup/payment-notice.popup.component';
+import { PaymentSuccessPopupComponent } from './components/payment-success-modal/payment-success-popup.component';
 import { RentalDetailsComponent } from './components/rental-details/rental-details.component';
 import {RentalsComponent} from './components/rentals/rentals.component'
 import { RentalsLayoutComponent } from "./layout/layout-rentals.component";
@@ -12,7 +14,7 @@ import { SharedModule } from "../shared/shared.module";
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @NgModule({
-  declarations: [PaymentComponent,RentalsComponent,RentalsLayoutComponent,RentalDetailsComponent,RequestRentalComponent,ReqRentConfirmPopupComponent],
+  declarations: [PaymentNoticePopupComponent,PaymentSuccessPopupComponent,PaymentComponent,RentalsComponent,RentalsLayoutComponent,RentalDetailsComponent,RequestRentalComponent,ReqRentConfirmPopupComponent],
   imports: [CommonModule, SharedModule, RentalsRoutings,SlickCarouselModule,FormsModule],
   providers: [],
 })
