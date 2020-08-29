2b541f5943b8190f6912b8e5ac9468ce39f76217
THIQAHDEVa.mabdelwahed <m.attia@outlook.sa>
2020-08-29 22:57:31 +0200

    add swet alert

--- src/app/rentals/components/request-rental/request-rental.component.ts
+++ src/app/rentals/components/request-rental/request-rental.component.ts
@@ -4,7 +4,7 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { GetRentalsListService } from "../../service/rental.service";
 import { RequestRentalModel } from '../../models/request-rental.model';
 import { RequestRentalService } from 'src/app/rentals/service/request-rental-service';
-import { Key } from 'protractor';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: "app-root",
@@ -212,12 +212,45 @@ export class RequestRentalComponent implements OnInit {
     }
   }
 
-   closeRequestModal(e){
+   public closeRequestModal(e){
     this.showRequestRentalconfirmation = e;
     this.showPaymentModal = true;
   }
 
-  closePaymentModal(e) {
+ public closePaymentModal(e) {
     this.showPaymentModal = false;
   }
+
+  // simpleAlert(){
+  //   Swal.fire('Hello world!');
+  // }
+
+  // alertWithSuccess(){
+  //   Swal.fire('Thank you...', 'You submitted succesfully!', 'success')
+  // }
+
+  // confirmBox(){
+  //   Swal.fire({
+  //     title: 'Are you sure want to remove?',
+  //     text: 'You will not be able to recover this file!',
+  //     icon: 'warning',
+  //     showCancelButton: true,
+  //     confirmButtonText: 'Yes, delete it!',
+  //     cancelButtonText: 'No, keep it'
+  //   }).then((result) => {
+  //     if (result.value) {
+  //       Swal.fire(
+  //         'Deleted!',
+  //         'Your imaginary file has been deleted.',
+  //         'success'
+  //       )
+  //     } else if (result.dismiss === Swal.DismissReason.cancel) {
+  //       Swal.fire(
+  //         'Cancelled',
+  //         'Your imaginary file is safe :)',
+  //         'error'
+  //       )
+  //     }
+  //   })
+  // }
 }
